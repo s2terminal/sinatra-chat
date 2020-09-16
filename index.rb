c442447043069fdef4ad8cf013c5ee0a5322af14
s2terminal <suzuki.sh@s2terminal.com>
2020-09-16 11:56:05 +0900

    add Chat

--- index.rb
+++ index.rb
@@ -6,5 +6,18 @@ configure do
 end
 
 get '/' do
-  'Hello Sinatra!'
+  @@chats ||= []
+  erb :index, locals: {
+    chats: @@chats.map{ |chat| add_suffix(chat) }.reverse
+  }
+end
+
+post '/' do
+  @@chats ||= []
+  @@chats.push({ content: params['content'], time: Time.now } )
+  redirect back
+end
+
+def add_suffix(chat)
+  { **chat, content: "#{chat[:content]}も" }
 end
