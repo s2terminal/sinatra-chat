843c5700f9665c44e89c74fc8af16710078b8689
suzuki.sh <suzuki.sh@s2terminal.com>
2020-09-16 13:10:25 +0900

    add MySQL

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM ruby:2.7-alpine
 WORKDIR /app
 
+RUN apk add --no-cache build-base mysql-dev
+
 COPY Gemfile ./
 COPY Gemfile.lock ./
-RUN bundle config set path 'vendor/bundle'
+RUN bundle config --local set path 'vendor/bundle'
 RUN bundle install
 
 CMD bundle exec ruby index.rb
--- index.rb
+++ index.rb
@@ -1,23 +1,65 @@
 require 'sinatra'
 require 'sinatra/reloader' if settings.development?
+require 'mysql2'
 
 configure do
   set :bind, '0.0.0.0'
 end
 
 get '/' do
-  @@chats ||= []
+  chats = chats_fetch
   erb :index, locals: {
-    chats: @@chats.map{ |chat| add_suffix(chat) }.reverse
+    chats: chats.map{ |chat| add_suffix(chat) }
   }
 end
 
 post '/' do
-  @@chats ||= []
-  @@chats.push({ content: params['content'], time: Time.now } )
+  chat_push(params['content'])
   redirect back
+end
+
+get '/initialize' do
+  client = Mysql2::Client.new(
+    :host => ENV['MYSQL_HOST'],
+    :username => ENV['MYSQL_USER'],
+    :password => ENV['MYSQL_PASS']
+  )
+  client.query("DROP DATABASE IF EXISTS #{ENV['MYSQL_DATABASE']}")
+  client.query("CREATE DATABASE IF NOT EXISTS #{ENV['MYSQL_DATABASE']}")
+  client = db_client
+  client.query(<<-EOS
+      CREATE TABLE IF NOT EXISTS chats (
+        id   INT AUTO_INCREMENT,
+        name TEXT,
+        content TEXT,
+        time DATETIME,
+        PRIMARY KEY(id)
+    )
+    EOS
+  )
+  redirect '/'
 end
 
 def add_suffix(chat)
   { **chat, content: "#{chat[:content]}も" }
+end
+
+def db_client()
+  Mysql2::Client.default_query_options.merge!(:symbolize_keys => true)
+  Mysql2::Client.new(
+    :host => ENV['MYSQL_HOST'],
+    :username => ENV['MYSQL_USER'],
+    :password => ENV['MYSQL_PASS'],
+    :database => ENV['MYSQL_DATABASE']
+  )
+end
+
+def chat_push(content, name="名無し")
+  db_client.prepare(
+    "INSERT into chats (name, content, time) VALUES (?, ?, NOW())"
+  ).execute(name, content)
+end
+
+def chats_fetch()
+  db_client.query("SELECT * FROM chats ORDER BY time DESC")
 end
